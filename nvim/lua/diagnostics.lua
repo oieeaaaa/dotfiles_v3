ed0dcdfd39096c41d2476356cef843584116a165
joe <joimee.cajandab@gmail.com>
2023-02-16 13:48:25 +0800

    :sparkles: auto show diagnostics on curshorhold

--- nvim/lua/diagnostics.lua
+++ nvim/lua/diagnostics.lua
@@ -2,5 +2,11 @@ vim.diagnostic.config({
   virtual_text = false,
 })
 
+vim.api.nvim_create_autocmd("CursorHold", {
+  group = general,
+  pattern = "*",
+  command = 'Lspsaga show_line_diagnostics ++unfocus',
+})
+
 -- show the full diagnostics in floating window
--- vim.o.updatetime = 100
+vim.o.updatetime = 100
