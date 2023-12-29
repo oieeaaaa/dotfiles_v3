6dedde1200cb919b2142aacb5fb83a7b711add7d
joe <joimee.cajandab@gmail.com>
2023-12-29 21:32:30 +0800

    BIG UPDATE: Packer to Lazy nvim migration

--- nvim/after/plugin/tree-sitter.lua
+++ nvim/after/plugin/tree-sitter.lua
@@ -1,42 +1,13 @@
 require('nvim-ts-autotag').setup()
 
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = {
-    "help",
-    "javascript",
-    "typescript",
-    "c",
-    "lua",
-    "rust",
-    "tsx",
-    "css",
-    "json",
-    "svelte",
-  },
-
   auto_install = true,
 
   highlight = {
     enable = true,
-
-    -- Disable highlighting for files larger than 100 KB
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
-    additional_vim_regex_highlighting = false,
+    disable = { "json" },
   },
 
-  indent = {
-    enable = true,
-  },
-
-  -- TODO: fix this auto close in react not working properly
   autotag = {
     enable = true,
     filetypes = {
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,73 +1,69 @@
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
 
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
   -- common
-  use 'tpope/vim-fugitive' -- Git commands
-  use 'nvim-lualine/lualine.nvim'
-  use 'lewis6991/gitsigns.nvim'
-  use 'edkolev/tmuxline.vim'
+  'tpope/vim-fugitive', -- Git commands
+  'nvim-lualine/lualine.nvim',
+  'lewis6991/gitsigns.nvim',
+  'edkolev/tmuxline.vim',
 
   -- color schemes
-  -- use 'gruvbox-community/gruvbox'
-  use 'folke/tokyonight.nvim'
-  -- use 'navarasu/onedark.nvim'
-  -- use ({ 'projekt0n/github-nvim-theme', tag = 'v0.0.7' })
-  -- use "olimorris/onedarkpro.nvim"
-  use "rebelot/kanagawa.nvim"
-  -- use 'xiyaowong/nvim-transparent'
-  -- use 'mhartington/oceanic-next'
-  use 'NvChad/nvim-colorizer.lua'
-  use 'folke/tokyonight.nvim'
+  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'NvChad/nvim-colorizer.lua',
+  'folke/tokyonight.nvim',
 
   -- general dev
-  use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-  }
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'glepnir/lspsaga.nvim', branch = 'main' }
-  use { 'numToStr/Comment.nvim' }
-  use('jose-elias-alvarez/null-ls.nvim')
-  use('MunifTanjim/prettier.nvim')
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+  {{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}},
+  { 'glepnir/lspsaga.nvim', branch = 'main' },
+  'numToStr/Comment.nvim',
+  'jose-elias-alvarez/null-ls.nvim',
+  'MunifTanjim/prettier.nvim',
 
   -- autocomplete
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'github/copilot.vim'
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  'github/copilot.vim',
 
   -- snippets
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'rafamadriz/friendly-snippets'
+  'L3MON4D3/LuaSnip',
+  'saadparwaiz1/cmp_luasnip',
+  'rafamadriz/friendly-snippets',
 
   -- autopairs
-  use 'tpope/vim-surround' -- surround characters shortcuts
-  use 'windwp/nvim-ts-autotag' -- pair jsx and html tags
+  'tpope/vim-surround', -- surround characters shortcuts
+  'windwp/nvim-ts-autotag', -- pair jsx and html tags
 
   -- search
-  use {
-    'nvim-telescope/telescope.nvim', requires = {
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'},
-    }
-  }
-  use 'tpope/vim-eunuch' -- wrappers UNIX commands
-  use 'tpope/vim-vinegar' -- file browser
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- for file icon
-    }
-  } -- file browser
-  use "folke/todo-comments.nvim"
+  'nvim-lua/popup.nvim',
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
+  'tpope/vim-eunuch', -- wrappers UNIX commands
+  'tpope/vim-vinegar', -- file browser
+  'kyazdani42/nvim-tree.lua',
+  'kyazdani42/nvim-web-devicons', -- for file icon
+
+  'folke/todo-comments.nvim',
 
   -- javascript
-  -- use { 'HerringtonDarkholme/yats.vim', ft = 'typescript' }
-  use { 'posva/vim-vue' } -- Vue highlighting
-  use 'othree/javascript-libraries-syntax.vim'
-  use 'MunifTanjim/eslint.nvim'
-end)
+  'othree/javascript-libraries-syntax.vim',
+  'MunifTanjim/eslint.nvim',
+})
